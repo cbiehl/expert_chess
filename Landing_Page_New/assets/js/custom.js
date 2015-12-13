a8ce5e15e2b013e3611c50bd0aacd19120fb34ee
cbiehl <D062629@192.168.0.15>
2015-12-13 13:16:49 +0100

    Enhancements n stuff

--- Landing_Page_New/assets/js/custom.js
+++ Landing_Page_New/assets/js/custom.js
@@ -1,15 +1,4 @@
 ﻿
-/*=============================================================
-    Authour URI: www.binarytheme.com
-    License: Commons Attribution 3.0
-
-    http://creativecommons.org/licenses/by/3.0/
-
-    100% Free To use For Personal And Commercial Use.
-    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
-   
-    ========================================================  */
-
 (function ($) {
     "use strict";
     var mainApp = {
@@ -46,24 +35,82 @@
              /*====================================
               FLEX SLIDER SCRIPTS 
              ======================================*/
-             $('#main-section').flexslider({
+        	 $('.iframe').click(function(){ //Attention: There is only 1 iframe in here!!
+             	 console.log("ER IST DRIN!");
+                 clicksOnYoutubePlugin += 1;
+             	 console.log(clicksOnYoutubePlugin);
+             	 if(clicksOnYoutubePlugin % 2 == 0){
+              		$('.flex-slider').flexslider('play');
+            	 }else{
+            		$('.flex-slider').flexslider('pause');
+             	 }
+              });
+        	 
+        	 
+        	var slider; //has 2 be global sagt Clemens
+        	var canSlide = true; // Global switch to monitor video state
+
+        	// Load the YouTube API. For some reason it's required to load it like this
+        	var tag = document.createElement('script');
+        	tag.src = "http://www.youtube.com/iframe_api";
+        	var firstScriptTag = document.getElementsByTagName('script')[0];
+        	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+        	// Setup a callback for the YouTube api to attach video event handlers
+        	window.onYouTubeIframeAPIReady = function(){
+        	    // Iterate through all videos
+        	    $('.iframe').each(function(){
+        	        // Create a new player pointer; "this" is a DOMElement of the player's iframe
+        	        var player = new YT.Player(this, {
+        	            playerVars: {
+        	                autoplay: 0
+        	            }
+        	        });
+
+        	        // Watch for changes on the player
+        	        player.addEventListener("onStateChange", function(state){
+        	            switch(state.data)
+        	            {
+        	                // If the user is playing a video, stop the slider
+        	                case YT.PlayerState.PLAYING:
+        	                	slider("stop");
+        	                    slider.flexslider("stop");
+        	                    canSlide = false;
+        	                    break;
+        	                // The video is no longer player, give the go-ahead to start the slider back up
+        	                case YT.PlayerState.ENDED:
+        	                case YT.PlayerState.PAUSED:
+        	                	slider("play");
+        	                    slider.flexslider("play");
+        	                    canSlide = true;
+        	                    break;
+        	            }
+        	        });
+
+        	        $(this).data('player', player);
+        	    });
+        	}
+        	 
+             slider = $('#main-section').flexslider({
                  animation: "fade", //String: Select your animation type, "fade" or "slide"
                  slideshowSpeed: 5000,           //Integer: Set the speed of the slideshow cycling, in milliseconds
                  animationSpeed: 800,           //Integer: Set the speed of animations, in milliseconds
                  startAt: 0,    //Integer: The slide that the slider should start on. Array notation (0 = first slide)
-
+                 pausePlay: true,
+                 pauseOnAction: true,
+                 video: true,
+                 before: function(){                     
+                     if(!canSlide)
+                         slider.flexslider("stop");
+                 }
              });
              
-             $('.iframe').click(function(){ //Attention: There is only 1 iframe in here!!
-             	 console.log("ER IST DRIN!");
-                 clicksOnYoutubePlugin += 1;
-             	 console.log(clicksOnYoutubePlugin);
-              	 if(clicksOnYoutubePlugin % 2 == 0){
-              		$('#main-section').flexslider('play');
-                	 }else{
-                		$('#main-section').flexslider('pause');
-             	 }
-              });
+             slider.on("click", ".flex-prev, .flex-next", function(){
+            	    canSlide = true;
+            	    $('.flexslider iframe').each(function(){
+            	        $(this).data('player').pauseVideo();
+            	    });
+            	});
              
          },
         
@@ -79,6 +126,17 @@
         mainApp.scroll_fun();
         mainApp.top_flex_slider_fun();
         mainApp.clemens_stuff();
+        
+        $('.iframe').click(function(){ //Attention: There is only 1 iframe in here!!
+        	console.log("ER IST DRIN!");
+            clicksOnYoutubePlugin += 1;
+        	console.log(clicksOnYoutubePlugin);
+    	 if(clicksOnYoutubePlugin % 2 == 0){
+         	$('.flex-slider').flexslider('play');
+       	 }else{
+       		$('.flex-slider').flexslider('pause');
+        	 }
+         });
     });
     
 }(jQuery));
