b38b72443d247352a4fd59917c3fda8323f99517
cbiehl <D062629@192.168.0.15>
2015-12-16 19:39:02 +0100

    Email verification!

--- Landing_Page_New/sendmail.php
+++ Landing_Page_New/sendmail.php
@@ -4,20 +4,22 @@
 	$from = "From: " . $_POST['name'] . "<" . $_POST['email'] . ">";
 	$text = $_POST['nachricht'];
 
-	/* email verification
+	/* email verification */
 	if (!(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))) {
-		header("Location: index.html");
-    	echo "<script>ValidateEmail(this.parentNode.)</script>"; fail, already redirected!
-    	exit;
-	}
-	*/
 	
-	mail($empfaenger, $betreff, $text, $from);
-
-	echo ("<SCRIPT>
+		echo ("<SCRIPT>
+	    	 window.location.href=\"index.html#contact-sec\";
+	    	 alert('Sorry, please enter a valid email address!');
+    	   </SCRIPT>");
+    	exit;
+    	
+   	}else{
+   		mail($empfaenger, $betreff, $text, $from);
+   		echo ("<SCRIPT>
 	    	 window.location.href=\"index.html#contact-sec\";
 	    	 //toastr[\"success\"](\"Your message has been sent to our Expert Developers!\");ICHWERDBEKLOPPTESGEHTNICHT
 	    	 alert('Your message has been sent to our Expert Developers!');
     	   </SCRIPT>");
+	}
     	   
 ?>
